e71681e93bb23e71dfb85b1d8ad8bc8a5e600f16
Luc Burns <burnsluc2@gmail.com>
2021-04-07 11:40:19 -0400

    Established req.body

--- server.js
+++ server.js
@@ -24,5 +24,14 @@ app.get("/notes", (req, res) => res.sendFile(path.join(__dirname, "public/notes.
 // A route for the notes api
 app.get("/api/notes", (req, res) => res.sendFile(path.join(__dirname, "db/db.json")));
 
+ // A route to post new notes
+app.post("/api/notes", (req, res) => {
+    const newNote = req.body;
+    let id = uniqid();
+    // console.log(newNote.title);
+    // console.log(newNote.text);
+})
+
 // Starts the server to begin listening
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
+
